a858930e8eece7fcd01cfc3fb8ab78c8b393398f
pppyyy <yokoryou25@gmail.com>
2026-08-25 20:23:11 +0900

    基本画面の作成
    ホーム画面:現在のタスク一覧と新しいタスクの作成、アルバムページの遷移
    かき氷画面:タイマー、スマホ振るボタン
    アルバム画面
    UIはノータッチ、画面遷移はあってるはず

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -5,26 +5,30 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:frontend/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('home, timer, and album flow works', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('こおり日和'), findsOneWidget);
+    expect(find.text('朝のストレッチ'), findsOneWidget);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.tap(find.text('朝のストレッチ'));
+    await tester.pumpAndSettle();
+    expect(find.text('とりかかる'), findsOneWidget);
+    expect(find.text('振って完成'), findsOneWidget);
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await tester.tap(find.text('振って完成'));
+    await tester.pumpAndSettle();
+    await tester.tap(find.text('アルバム'));
+    await tester.pumpAndSettle();
+    expect(find.text('朝のストレッチ'), findsOneWidget);
   });
 }

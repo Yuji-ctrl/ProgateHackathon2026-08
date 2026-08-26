bb70d967641e74a5366b04d6786a80d05f75b949
tomoodi <usagichan15go@icloud.com>
2026-08-26 22:28:17 +0900

    supabaseの接続

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,17 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://tnsnwhicteyxcsqlhfcw.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRuc253aGljdGV5eGNzcWxoZmN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2NjE2ODcsImV4cCI6MjEwMzIzNzY4N30.8tpIrG_0YXsbJZRQd_7oT4UCC02nrCpqHi5CppMvtZ8',
+  );
+
+  runApp(const MyApp());
+}
 
 class Task {
   Task(this.title, this.category, this.minutes, {this.detail = '', this.ghost = false});
